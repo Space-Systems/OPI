bed9824f05954e352942494b5e21acc145e73e2a
Patrick Thomsen <ilr-opi@p-thomsen.de>
2014-01-30 16:11:34 +0100

    added license header

--- src/OPI/internal/opi_plugin.h
+++ src/OPI/internal/opi_plugin.h
@@ -1,9 +1,19 @@
-/***********************************************************************
- * This is an internal header
+/* OPI: Orbital Propagation Interface
+ * Copyright (C) 2014 Institute of Aerospace Systems, TU Braunschweig, All rights reserved.
  *
- * It Provides an easy interface to load all common plugin functions
- ***********************************************************************/
-
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
 #ifndef OPI_PLUGIN_H
 #define OPI_PLUGIN_H
 #include <string>
