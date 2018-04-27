1a343c7ff2385dcb90481284435173a060355861
Marek Moeckel <marekmoeckel@serc.org.au>
2018-04-27 15:55:13 +1000

    Additional functions for V matrix handling

--- src/OPI/opi_datatypes.h
+++ src/OPI/opi_datatypes.h
@@ -269,6 +269,55 @@ namespace OPI
         else return std::numeric_limits<double>::quiet_NaN();
     }
 
+    //! Addition assignment operator for VMatrix
+    OPI_CUDA_PREFIX inline VMatrix& operator+=(VMatrix& a, const VMatrix& b)
+    {
+        a.accX_posX += b.accX_posX;
+        a.accY_posX += b.accY_posX;
+        a.accZ_posX += b.accZ_posX;
+        a.accX_posY += b.accX_posY;
+        a.accY_posY += b.accY_posY;
+        a.accZ_posY += b.accZ_posY;
+        a.accX_posZ += b.accX_posZ;
+        a.accY_posZ += b.accY_posZ;
+        a.accZ_posZ += b.accZ_posZ;
+
+        a.accX_velX += b.accX_velX;
+        a.accY_velX += b.accY_velX;
+        a.accZ_velX += b.accZ_velX;
+        a.accX_velY += b.accX_velY;
+        a.accY_velY += b.accY_velY;
+        a.accZ_velY += b.accZ_velY;
+        a.accX_velZ += b.accX_velZ;
+        a.accY_velZ += b.accY_velZ;
+        a.accZ_velZ += b.accZ_velZ;
+        return a;
+    }
+
+    //! writes the VMatrix to a C vector (no bounds check!)
+    OPI_CUDA_PREFIX inline void vMatrixToArray(const VMatrix m, double* v)
+    {
+        v[0] = m.accX_posX;
+        v[1] = m.accY_posX;
+        v[2] = m.accZ_posX;
+        v[3] = m.accX_posY;
+        v[4] = m.accY_posY;
+        v[5] = m.accZ_posY;
+        v[6] = m.accX_posZ;
+        v[7] = m.accY_posZ;
+        v[8] = m.accZ_posZ;
+
+        v[9] = m.accX_velX;
+        v[10] = m.accY_velX;
+        v[11] = m.accZ_velX;
+        v[12] = m.accX_velY;
+        v[13] = m.accY_velY;
+        v[14] = m.accZ_velY;
+        v[15] = m.accX_velZ;
+        v[16] = m.accY_velZ;
+        v[17] = m.accZ_velZ;
+    }
+
 }
 
 #endif
