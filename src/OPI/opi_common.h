de8740781eae006ef5915b0e4053ce302e558048
mmoeckel <marekmoeckel@serc.org.au>
2020-06-15 15:37:04 +1000

    Use correct version number

--- src/OPI/opi_common.h
+++ src/OPI/opi_common.h
@@ -1,9 +1,9 @@
 #ifndef OPI_COMMON_COMMON_H
 #define OPI_COMMON_COMMON_H
 
-const int OPI_API_VERSION_MAJOR = 0;
-const int OPI_API_VERSION_MINOR = 1;
-#if WIN32
+const int OPI_API_VERSION_MAJOR = 2019;
+const int OPI_API_VERSION_MINOR = 6;
+#ifdef WIN32
 #ifdef OPI_COMPILING_DYNAMIC_LIBRARY
 #define OPI_API_EXPORT __declspec( dllexport )
 #else
