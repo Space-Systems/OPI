6701c6dae4becd5ed9aa720e0274b2e0fe52be78
Marek Moeckel <marekmoeckel@serc.org.au>
2020-08-20 09:52:18 +1000

    Update version number

--- src/OPI/opi_common.h
+++ src/OPI/opi_common.h
@@ -2,7 +2,7 @@
 #define OPI_COMMON_COMMON_H
 
 const int OPI_API_VERSION_MAJOR = 2019;
-const int OPI_API_VERSION_MINOR = 7;
+const int OPI_API_VERSION_MINOR = 8;
 #ifdef WIN32
 #ifdef OPI_COMPILING_DYNAMIC_LIBRARY
 #define OPI_API_EXPORT __declspec( dllexport )
