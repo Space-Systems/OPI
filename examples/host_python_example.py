5eeb2f03c9c1f0755b9848d2694d5c298299d03e
Marek Moeckel <marekmoeckel@serc.org.au>
2021-02-10 15:30:32 +1100

    Update Python host example

--- examples/host_python_example.py
+++ examples/host_python_example.py
@@ -4,71 +4,78 @@
 # a system path, you can use the LD_LIBRARY_PATH variable, e.g.
 # LD_LIBRARY_PATH=</path/to/opi/installation>/lib python host_python_example.py
 
-# Import bindings from opi.py (created with the included shell script).
-from opi import *
+# Import bindings from OPI.py (needs to be in your PYTHONPATH)
+import OPI
 
 # Create host object
-host = OPI_createHost()
+host = OPI.Host()
 
 # Load plugins from given directory
-OPI_loadPlugins(host, "plugins")
-
-# Create population data with 200 objects (must be done after loading the plugins)
-data = OPI_createData(host, 200)
-
-# Fetch a pointer to the orbit data of the population we just created
-orbits = OPI_Population_getOrbit(data)
-
-# Fetch a pointer to the object properties array of the population
-props = OPI_Population_getObjectProperties(data)
-
-# Set initial values for objects
-# Don't forget setting BOL and EOL to zero if you don't use them!
-# Also remember to set sensible values for the other properties
-# as leaving them uninitialized can lead to strange propagation
-# results. This is especially true for area to mass ratio, drag
-# coefficient and reflectivity in combination with atmospheric
-# perturbation models.
-for i in range(0, OPI_Population_getSize(data)):
-	orbits[i].semi_major_axis = 6800
-	orbits[i].eccentricity = 0.001
-	orbits[i].inclination = 23.5
-	orbits[i].raan = 0
-	orbits[i].arg_of_perigee = 0
-	orbits[i].mean_anomaly = 0
-	orbits[i].bol = 0
-	orbits[i].eol = 0
-	props[i].id = i
-	props[i].mass = 100
-	props[i].diameter = 10
-	props[i].area_to_mass = 1
-	props[i].drag_coefficient = 2.2
-	props[i].reflectivity = 1.3
-
-# Notify OPI of the updated values
-OPI_Population_update(data, OPI_DEVICE_HOST)
+host.loadPlugins("plugins", OPI.Host.PLATFORM_NONE)
 
 # Print information about available propagators
-print "Propagators:"
-for i in range(0, OPI_getPropagatorCount(host)):
-	name = OPI_Module_getName(OPI_getPropagatorByIndex(host, i))
-	author = OPI_Module_getAuthor(OPI_getPropagatorByIndex(host, i))
-	print "#",i,": ",name," by ",author
+print("Propagators:")
+for i in range(0, host.getPropagatorCount()):
+    name = host.getPropagator(i).getName()
+    author = host.getPropagator(i).getAuthor()
+    print("  #",i,":",name,"by",author)
+
+# Create a population with one object
+population = OPI.Population(host,1)
+
+# Set object's orbit
+orbit = OPI.Orbit()
+orbit.semi_major_axis = 7150.59176
+orbit.eccentricity = 0.0012347
+orbit.inclination = 1.022
+orbit.raan = 0.3
+orbit.arg_of_perigee = 0.66
+orbit.mean_anomaly = 0.1
+population.setOrbit(0, orbit)
+
+# If the propagator needs state vectors, use this instead:
+#position = OPI.Vector3(615.119526, -7095.644839, -678.668352)
+#velocity = OPI.Vector3(0.390367, 0.741902, -7.396980)
+#population.setPosition(0, position)
+#population.setVelocity(0, velocity)
+
+# Set object's properties
+props = OPI.ObjectProperties()
+props.mass = 1.3
+props.area_to_mass = 1.0
+props.diameter = 0.01
+props.drag_coefficient = 2.2
+props.reflectivity = 1.3
+population.setObjectProperties(0, props)
+
+# Tell OPI about the population updates
+#population.update(OPI.DATA_POSITION, OPI.DEVICE_HOST)
+#population.update(OPI.DATA_VELOCITY, OPI.DEVICE_HOST)
+population.update(OPI.DATA_ORBIT, OPI.DEVICE_HOST)
+population.update(OPI.DATA_PROPERTIES, OPI.DEVICE_HOST)
 
 # Select a propagator by name
-propagator = OPI_getPropagatorByName(host, "Fortran Example Propagator")
+propagator = host.getPropagator("BasicCPP")
 
 # Start propagation if a valid propagator was selected
 if propagator:
-	# Propagate all objects with the chosen propagator and the given time stamp and step size
-	OPI_Propagator_propagateAll(propagator, data, 2450980.0, 86400.0)
-	# Update orbit pointer 
-	orbits = OPI_Population_getOrbit(data)
-	# Print (partial) propagation results for one object
-	print(str(orbits[1].semi_major_axis) + " " + str(orbits[1].eccentricity) + " " + str(orbits[1].mean_anomaly))
+    propagator.enable()
+    # Set start date and step size
+    startDate = 2440980.0
+    stepSize = 60.0
+
+    # Propagation loop
+    for i in range(0,5):
+            propagationTime = startDate + ((i*stepSize)/86400.0)
+            propagator.propagate(population, propagationTime, stepSize)
+            p = population.getPositionByIndex(0)
+            print(p.x, p.y, p.z)
+
+    # Disable propagator
+    propagator.disable()
 else:
-	print("Propagator not found!")
+    print("Propagator not found!")
 
 # Clean up
-OPI_destroyData(data)
-OPI_destroyHost(host)
+del population
+del propagator
